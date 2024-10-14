d1fbba8bfaf3177b3f21dde8ed9ae4f0389fc866
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-10-14 15:02:50 +0300

    Updated resume website

--- script.js
+++ script.js
@@ -9,11 +9,3 @@ document.querySelector('a[href^="#"]').forEach(anchor=> {
 })
 
 // Toggle navigation for mobile
-function toggleNav() {
-     const navList = document.getElementById('navList');
-     if (navList.style.display === 'block') {
-         navList.style.display = 'none';
-     } else {
-         navList.style.display = 'block';
-     }
-}
